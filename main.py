7e8c6a9d0450209837a8dc24188b068f050d9c5f
EyuAtske <EyuelA.Fekade21@gmail.com>
2025-10-31 09:41:45 +0300

    the player can move now

--- main.py
+++ main.py
@@ -7,13 +7,19 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)    
         screen.fill("black")
-        player.draw(screen)
+        for draw in drawable:
+            draw.draw(screen)
         dt = clock.tick(60)/1000
         pygame.display.flip()
 
